417486182e212698400151e3292f355ddf62daff
Deepak Jayaram <dpk@Deepaks-MacBook-Pro.local>
2017-03-03 00:49:51 -0500

    Wiring up MongoDB to the post method and creating a configs.json to store db credentials

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
 const express = require('express');
 const path = require('path');
 const bodyParser= require('body-parser');
-const MongoClient = require('mongodb').MongoClient
+const MongoClient = require('mongodb').MongoClient;
+var config = require('./config.json');
+var db;
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3000, ()=> {
-    console.log("listening on port 3000");
-});
-
 app.get("/", (req, res)=> {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.post("/names", (req, res) => {
-    console.log(req.body);
+    db.collection('names').save(req.body, (err, result) => {
+        if (err) return console.log(err)
+        console.log('saved to database')
+        res.redirect('/')
+    })
 });
+
+MongoClient.connect(`mongodb://${config.db.username}:${config.db.password}@ds133328.mlab.com:33328/test-db-crud`, (err, database) => {
+    if (err) return console.log(err);
+    db = database
+    app.listen(3000, () => {
+        console.log("listening on port 3000");
+    });
+})
