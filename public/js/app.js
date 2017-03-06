68fc1f7bd77d25951943e87149009966d20b54e7
Deepak Jayaram <dpk@Deepaks-MacBook-Pro.local>
2017-03-05 23:30:50 -0500

    Adding a modal for delete record

--- public/js/app.js
+++ public/js/app.js
@@ -8,3 +8,9 @@ $(".item-edit-cancel").on("click", function() {
     var $parentListItemContainer = $(this).parents("li");
     $parentListItemContainer.attr("data-isediting", false);
 });
+
+$(".delete-item").on("click", function() {
+     var $nameText = $(this).parents(".btn-group").siblings(".name-item").text();
+     console.log($nameText);
+     $("#deleteModal").find(".modal-name-item").html($nameText);
+})
